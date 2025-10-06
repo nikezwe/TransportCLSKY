2fecc5e0542ff38fe6eff83c593163be717d7c38
nikezwe <mnikezwe@gmail.com>
2025-10-06 15:21:03 +0200

    fix error

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <title>@yield('title', 'CL SKY - Transport Chine-Burundi')</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="{{ asset('css/user.css') }}" rel="stylesheet">
     @stack('head')
 </head>
